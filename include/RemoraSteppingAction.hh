4544f98ff9d4c79df1264702deabd36c4b31531a
John Francis <john9francis@gmail.com>
2024-07-24 21:42:47 +0000

    Setup the remora stepping action class as a layer on top of another user stepping action

---
+++ include/RemoraSteppingAction.hh
@@ -0,0 +1,22 @@
+#ifndef REMORA_STEPPING_ACTION_HH
+#define REMORA_STEPPING_ACTION_HH 1
+
+#include "G4UserSteppingAction.hh"
+
+namespace remora {
+
+class RemoraSteppingAction : public G4UserSteppingAction {
+public:
+  RemoraSteppingAction(G4UserSteppingAction* prevAction=nullptr);
+  ~RemoraSteppingAction();
+
+  void SetSteppingManagerPointer(G4SteppingManager* pValue) override;
+  void UserSteppingAction(const G4Step*) override;
+
+private:
+  G4UserSteppingAction* pPrevAction = nullptr;
+};
+
+} // !namespace remora
+
+#endif
---
+++ src/RemoraSteppingAction.cc
@@ -0,0 +1,23 @@
+#include "RemoraSteppingAction.hh"
+
+namespace remora
+{
+
+RemoraSteppingAction::RemoraSteppingAction(G4UserSteppingAction* prevAction)
+: pPrevAction(prevAction) {}
+
+RemoraSteppingAction::~RemoraSteppingAction(){
+  if (pPrevAction) delete pPrevAction;
+}
+
+void RemoraSteppingAction::SetSteppingManagerPointer(G4SteppingManager* pValue){
+  if (pPrevAction) pPrevAction->SetSteppingManagerPointer(pValue);
+}
+
+void RemoraSteppingAction::UserSteppingAction(const G4Step* aStep){
+  if (pPrevAction) pPrevAction->UserSteppingAction(aStep);
+}
+
+
+  
+} // namespace remora
