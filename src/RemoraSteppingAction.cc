81d3d2d89d81c1bdfd3834a64f79809b8a956070
John Francis <john9francis@gmail.com>
2024-07-25 03:50:27 +0000

    Readjusted the logic so that we get all points in the track. when the track is added for the first time, we get the pre-step point. then, every other time, we get the second point.

--- src/RemoraSteppingAction.cc
+++ src/RemoraSteppingAction.cc
@@ -27,9 +27,12 @@ void RemoraSteppingAction::UserSteppingAction(const G4Step* aStep){
   if (!pTrajManager->Exists(id)){
     G4String name = aStep->GetTrack()->GetParticleDefinition()->GetParticleName();
     pTrajManager->AddTraj(id, name);
+    // add the first point (pre)
+    pTrajManager->AddPoint(id, G4ThreeVector(aStep->GetPreStepPoint()->GetPosition()));
   }
 
-  pTrajManager->AddPoint(id, G4ThreeVector(aStep->GetPreStepPoint()->GetPosition()));
+  // from now on, add the post step points
+  pTrajManager->AddPoint(id, G4ThreeVector(aStep->GetPostStepPoint()->GetPosition()));
 
   if (aStep->GetTrack()->GetTrackStatus() != fAlive){
     pTrajManager->FinishTraj(id);
