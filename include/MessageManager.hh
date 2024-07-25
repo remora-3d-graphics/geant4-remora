e5e06043cf7291ec5a91f19976e8249cb5402a33
John Francis <john9francis@gmail.com>
2024-07-25 19:16:47 +0000

    Set up the message manager class

--- include/MessageManager.hh
+++ include/MessageManager.hh
@@ -1,13 +1,31 @@
 #ifndef REMORA_MESSAGE_MANAGER_HH
 #define REMORA_MESSAGE_MANAGER_HH 1
 
-namespace remora
-{
+#include <queue>
+#include <string>
+#include <unordered_map>
+
+namespace remora {
   
 class MessageManager {
+public:
+  MessageManager();
+
+  void AddNewClient(int clientID);
+  void RemoveClient(int clientID);
+  bool ClientExists(int clientID);
+
+  bool QueueMessage(int clientID);
+  bool QueueMessageForAll();
+
+  std::string GetNextMessage(int clientID);
+  void MessageSent(int clientID);
+
+private:
+  std::unordered_map<int, std::queue<std::string>> clientMap;
 
 };
 
-} // namespace remora
+} // !namespace remora
 
 #endif;
---
+++ src/MessageManager.cc
@@ -0,0 +1,2 @@
+#include "MessageManager.hh"
+
